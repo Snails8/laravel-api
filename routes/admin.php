881d54d8e643e71f96b30ef98f7c90558447ee97
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-20 12:16:58 +0900

    管理画面認証用のroute追加

--- routes/admin.php
+++ routes/admin.php
@@ -2,5 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\Auth\LoginController;
 
-Route::get('/home', [HomeController::class, 'index'])->name('admin.home');
+Route::get('login', [LoginController::class, 'showLoginForm']);
+Route::post('login', [LoginController::class, 'login']);
+
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('admin.home');
+});
