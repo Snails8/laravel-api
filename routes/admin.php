9137a22a0a7c69509b1218ea2c50a2c6f73715e3
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-20 17:10:17 +0900

    管理画面認証作成

--- routes/admin.php
+++ routes/admin.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\Auth\LoginController;
 
-Route::get('login', [LoginController::class, 'showLoginForm']);
-Route::post('login', [LoginController::class, 'login']);
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login'])->name('admin.login');
 
 Route::middleware('auth:admin')->group(function () {
+
     Route::get('/home', [HomeController::class, 'index'])->name('admin.home');
 });
