ddceb0092928bf5c7e0437ddd0fe8945de7cda87
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-20 17:22:59 +0900

    layoutのリファクタリングとlogoutのルート追加

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\Auth\LoginController;
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [LoginController::class, 'login'])->name('admin.login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth:admin')->group(function () {
 
