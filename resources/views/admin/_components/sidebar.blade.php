6778e80a1eeeacbd8151ea0a36368bc0325fd902
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-20 19:53:59 +0900

    不要な処理の削除

--- resources/views/admin/_components/sidebar.blade.php
+++ resources/views/admin/_components/sidebar.blade.php
@@ -21,29 +21,6 @@
       </li>
     </ul>
 
-{{--    @if(Auth::user()->role === 'Master' || Auth::user()->role === 'Standard')--}}
-{{--      <h6 class="sidebar-heading px-3 mt-4 mb-1 text-muted">業者管理</h6>--}}
-{{--      <ul class="nav flex-column mb-2">--}}
-{{--        <li class="nav-item pl-2">--}}
-{{--          <a class="nav-link" href="{{ route('admin.home') }}">--}}
-{{--            <span data-feather="chevron-right"></span>--}}
-{{--            業者マスタ--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--        <li class="nav-item pl-2">--}}
-{{--          <a class="nav-link " href="{{ route('admin.estate_confirm.index') }}">--}}
-{{--            <span data-feather="chevron-right"></span>--}}
-{{--            物件確認--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--        <li class="nav-item pl-2">--}}
-{{--          <a class="nav-link" href="{{ route('admin.shop_agent.index') }}">--}}
-{{--            <span data-feather="chevron-right"></span>--}}
-{{--            物件確認設定<!--（FAX以外）-->--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--      </ul>--}}
-
       <h6 class="sidebar-heading px-3 mt-4 mb-1 text-muted">HP管理</h6>
       <ul class="nav flex-column mb-2">
         <li class="nav-item pl-2">
