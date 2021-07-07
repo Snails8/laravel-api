40a9f8ebdf8505dd39167869776dc0cac7f567d5
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-07 20:39:21 +0900

    guradの追加

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Company
+ * @package App\Models
+ */
 class Company extends Model
 {
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     use HasFactory;
 }
