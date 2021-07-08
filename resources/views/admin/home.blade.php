0f7351f94565b2e8d15d6526e3c00ee714f0d777
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-08 11:37:15 +0900

    add: 基本スタイルの作成

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,43 +1,43 @@
-{{--@extends('admin._layouts.layout')--}}
-
-{{--@section('content')--}}
-{{--  <div class="container">--}}
-{{--    <div class="row justify-content-center">--}}
-{{--      <div class="col-md-8">--}}
-{{--        <div class="card">--}}
-{{--          <div class="card-header">{{ __('Dashboard') }}</div>--}}
-
-{{--          <div class="card-body">--}}
-{{--            @if (session('status'))--}}
-{{--              <div class="alert alert-success" role="alert">--}}
-{{--                {{ session('status') }}--}}
-{{--              </div>--}}
-{{--            @endif--}}
-
-{{--            {{ __('You are logged in!') }}--}}
-{{--          </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
-{{--    </div>--}}
-{{--  </div>--}}
-{{--@endsection--}}
-
-@extends('adminlte::page')
-
-@section('title', 'Dashboard')
-
-@section('content_header')
-  <h1>Dashboard</h1>
-@stop
+@extends('admin._layouts.layout')
 
 @section('content')
-  <p>Welcome to this beautiful admin panel.</p>
-@stop
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">{{ __('Dashboard') }}</div>
 
-@section('css')
-  <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
+          <div class="card-body">
+            @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+            @endif
 
-@section('js')
-  <script> console.log('Hi!'); </script>
-@stop
+            {{ __('You are logged in!') }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+{{--@extends('adminlte::page')--}}
+
+{{--@section('title', 'Dashboard')--}}
+
+{{--@section('content_header')--}}
+{{--  <h1>Dashboard</h1>--}}
+{{--@stop--}}
+
+{{--@section('content')--}}
+{{--  <p>Welcome to this beautiful admin panel.</p>--}}
+{{--@stop--}}
+
+{{--@section('css')--}}
+{{--  <link rel="stylesheet" href="/css/admin_custom.css">--}}
+{{--@stop--}}
+
+{{--@section('js')--}}
+{{--  <script> console.log('Hi!'); </script>--}}
+{{--@stop--}}
