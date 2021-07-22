213ee68a38d3f7c9750aef94b9c2cdb1569c117f
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-22 09:55:00 +0900

    add: 契約判定のコントローラー追加

--- routes/ajax.php
+++ routes/ajax.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Ajax\CompanyController;
 
 Route::prefix('companies')->group(function () {
-    Route::put('/{company}/is_contract');
+    Route::put('/{company}/is_contract', [CompanyController::class, 'updateContractStatus']);
 });
