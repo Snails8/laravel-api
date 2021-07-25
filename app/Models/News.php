c475158b9db51bed1c4156d3e6c967e386f54c85
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-25 20:55:34 +0900

    News とのリレーション設定追加

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class News extends Model
 {
     use HasFactory;
+
+    /**
+     * NewsCategory とのリレーション
+     * @return BelongsTo
+     */
+    public function newsCategory(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\NewsCategory');
+    }
 }
--- app/Models/NewsCategory.php
+++ app/Models/NewsCategory.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class NewsCategory extends Model
 {
     use HasFactory;
+
+    /**
+     * News とのリレーション
+     * @return HasMany
+     */
+    public function news(): HasMany
+    {
+        return $this->hasMany('App\Models\News');
+    }
 }
