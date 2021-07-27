c51651434db6206b96488da3da43f0cb3c329795
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-27 09:28:47 +0900

    add: sidebar に動線追加

--- resources/views/admin/_partials/sidebar.blade.php
+++ resources/views/admin/_partials/sidebar.blade.php
@@ -23,5 +23,21 @@
         <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>サブメニュー3</a></li>
       </ul>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item2" aria-controls="menu_item2" aria-expanded="true" href="{{ route('admin.news.index') }}">
+        お知らせ管理<i class="bi bi-caret-down-fill ms-1"></i>
+      </a>
+      <ul id="menu_item2" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
+        <li><a class="nav-link" href="{{ route('admin.news_category') }}"><i class="bi bi-chevron-right me-1"></i>お知らせカテゴリ管理</a></li>
+      </ul>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item2" aria-controls="menu_item2" aria-expanded="true" href="{{ route('admin.company.index') }}">
+        自社情報管理<i class="bi bi-caret-down-fill ms-1"></i>
+      </a>
+      <ul id="menu_item2" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
+        <li><a class="nav-link" href="{{ route('admin.news_category') }}"><i class="bi bi-chevron-right me-1"></i>ユーザー管理</a></li>
+      </ul>
+    </li>
   </ul>
 </nav>
