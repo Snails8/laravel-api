1808a1ea06be9e5415df9d4bff77da7b01e9ad3c
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-28 18:29:58 +0900

    add: sidebar に動線追加

--- resources/views/admin/_partials/sidebar.blade.php
+++ resources/views/admin/_partials/sidebar.blade.php
@@ -39,5 +39,13 @@
         <li><a class="nav-link" href="{{ route('admin.company.index') }}"><i class="bi bi-chevron-right me-1"></i>ユーザー管理</a></li>
       </ul>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item2" aria-controls="menu_item2" aria-expanded="true" href="{{ route('admin.hr_company.index') }}">
+        利用会社管理<i class="bi bi-caret-down-fill ms-1"></i>
+      </a>
+      <ul id="menu_item2" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
+        <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>利用ユーザー管理</a></li>
+      </ul>
+    </li>
   </ul>
 </nav>
