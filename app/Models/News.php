7b812f201fdd76a895760332f8e432bcad466dce
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-28 21:31:08 +0900

    fix: 適切に保存されていなかったので修正

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,9 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * お知らせ
+ * Class News
+ * @package App\Models
+ */
 class News extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 
     /**
      * NewsCategory とのリレーション
