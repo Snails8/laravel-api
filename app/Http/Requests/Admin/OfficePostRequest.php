30ae31882b17bef9d1d5884916044e9d18d3b7bf
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-29 09:34:54 +0900

    add: 支社管理のCRUD 作成

--- app/Http/Requests/Admin/OfficePostRequest.php
+++ app/Http/Requests/Admin/OfficePostRequest.php
@@ -13,7 +13,12 @@ class OfficePostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        $paths = [
+            'shop.store',
+            'shop.update',
+        ];
+
+        return (in_array($this->route()->action['as'], $paths));
     }
 
     /**
@@ -23,8 +28,39 @@ class OfficePostRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
+        $rules = [
+            'name'              => 'required|string|max:255',
+            'address'           => 'required|string|max:255',
+            'tel'               => 'required|regex:/^[0-9]{2,4}[0-9]{2,4}[0-9]{3,4}$/',
+            'manager'           => 'required|string|max:255',
+            'post'              => 'required|string|max:255',
         ];
+
+        return $rules;
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function messages()
+    {
+        $messages = [
+            'name.required'              => '名前は必ず入力してください。',
+            'name.string'                => '数字や記号は除外してください。',
+            'name.max'                   => '255文字以内で入力してください。',
+            'address.required'           => '住所は必ず入力してください。',
+            'address.string'             => '数字や記号は除外してください。',
+            'address.max'                => '255文字以内で入力してください。',
+            'tel.required'               => '電話番号は必ず入力してください。',
+            'tel.regex'                  => '有効な電話番号の形式ではありません。',
+            'manager.required'           => '代表者名は必ず入力してください。',
+            'manager.string'             => '数字や記号は除外してください。',
+            'manager.max'                => '255文字以内で入力してください。',
+            'post.required'              => '代表者名は必ず入力してください。',
+            'post.string'                => '数字や記号は除外してください。',
+            'post.max'                   => '255文字以内で入力してください。',
+        ];
+
+        return $messages;
     }
 }
