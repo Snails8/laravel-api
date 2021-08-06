81b7a6591053632405797edb08fdf5f2f7c9ae1c
S-tanishi <tanichi.u.n.20@gmail.com>
2021-08-06 21:16:25 +0900

    fix:bootstrap5 以降に伴う修正と処理見直し

--- resources/views/admin/news_categories/_form.blade.php
+++ resources/views/admin/news_categories/_form.blade.php
@@ -2,14 +2,14 @@
 <table class="table table-striped border item-va-middle table-form">
   <tbody>
   <tr>
-    <th class="bg-dark text-white border-white"><span class="p-2 mr-2 badge badge-danger">必須</span>お知らせカテゴリ名</th>
+    <th class="bg-dark text-white"><span class="p-2 me-2 badge bg-danger">必須</span>お知らせカテゴリ名</th>
     <td>
       @includeWhen($errors->get('name'), 'admin._partials.validation_error', ['errors' => $errors->get('name')])
       {{ Form::text('name', old('name', $newsCategory->name), ['class' => 'form-control', 'required' => 'required']) }}
     </td>
   </tr>
   <tr>
-    <th class="bg-dark text-white border-white">優先順位</th>
+    <th class="bg-dark text-white">優先順位</th>
     <td>
       @includeWhen($errors->get('sort_no'), 'admin._partials.validation_error', ['errors' => $errors->get('sort_no')])
       {{ Form::text('sort_no', old('sort_no', $newsCategory->sort_no ?: 50), ['class' => 'form-control']) }}
--- resources/views/admin/news_categories/index.blade.php
+++ resources/views/admin/news_categories/index.blade.php
@@ -2,9 +2,8 @@
 @section('title', $title)
 @section('content')
   @includeWhen(session('flash_message'), 'admin._partials.flash_message_success')
-  <h4 class="c-grey-900 mT-10 mB-30">{{ $title }}</h4>
   {{ Form::open(['route' => ['admin.news_category.index'], 'class' => 'pb-3 border-bottom', 'method' => 'GET']) }}
-  <table class="table table-striped table-bordered table-search">
+  <table class="table table-striped table-bordered align-middle">
     <colgroup>
       <col style="width: 12%;">
       <col style="width: 33%;">
@@ -14,12 +13,12 @@
     <tr>
       <th scope="row" class="bg-dark text-white">お知らせカテゴリ名</th>
       <td colspan="3">
-        {{ Form::text('keyword', isset($params['keyword']) ? $params['keyword'] : '', ['class' => 'form-control']) }}
+        {{ Form::text('keyword', isset($params['keyword']) ?? '', ['class' => 'form-control']) }}
       </td>
     </tr>
   </table>
   <div class="text-center">
-    {{ Form::submit('検索', ['class' => 'btn btn-primary pl-5 pr-5']) }}
+    {{ Form::submit('検索', ['class' => 'btn btn-primary px-5']) }}
   </div>
   {{ Form::close() }}
 
@@ -27,8 +26,8 @@
   <div id='search_result' class="operation mt-3 py-3">
     <a class="btn btn-outline-primary" href="{{ route('admin.news_category.create') }}">新規作成</a>
   </div>
-  <table class="table table-striped table-bordered item-va-middle table-list">
-    <thead class="thead-dark">
+  <table class="table table-striped table-bordered table-search">
+    <thead class="bg-dark text-white">
     <tr>
       <th scope="col" class="id">ID</th>
       <th scope="col">お知らせカテゴリ名</th>
