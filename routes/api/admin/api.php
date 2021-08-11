b5d4b0f13ad69c6b8d6f19f968fe85cde753b6cf
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-08-12 08:43:38 +0900

    add: ApiAdmin のLogin 処理の作成

--- routes/api/admin/api.php
+++ routes/api/admin/api.php
@@ -2,19 +2,18 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\Api\HrAdmin\Auth\LoginController;
+use App\Http\Controllers\Api\Admin\Auth\LoginController;
 use App\Http\Controllers\Api\HrAdmin\UserController;
 
+// 認証処理
 Route::prefix('auth')->group(function () {
-//    Route::post('/login', [LoginController::class, 'login']);
-//    Route::get('logout', [LoginController::class, 'logout']);
+   Route::post('/login', [LoginController::class, 'login']);
+   Route::get('logout', [LoginController::class, 'logout']);
 });
 
-Route::get('/users', [UserController::class, 'getHrUsers']);
-
-//Route::middleware('auth:api_hr_admin')->group(function () {
-//     認証判定
-//    Route::get('/user', function () {
-//        return Auth::user();
-//    });
-//});
+Route::middleware('auth:api_admin')->group(function () {
+   // 認証判定
+   Route::get('/user', function () {
+       return Auth::user();
+   });
+});
