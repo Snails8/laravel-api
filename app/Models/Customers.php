2310dfc4b7b5b62099c901a0aa602746fba4a4fd
S-tanishi <tanichi.u.n.20@gmail.com>
2021-08-17 21:23:23 +0900

    add: 認証に対応したModelの作成

---
+++ app/Models/Customers.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+/**
+ * 表側のlogin に使用する会員データのモデルy
+ */
+class Customers extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
