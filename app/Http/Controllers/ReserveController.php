2bf9bb1cb0bebc6fa5b028b9956996a5f6e0d37e
S-tanishi <tanichi.u.n.20@gmail.com>
2021-08-28 21:44:55 +0900

    addd: mail 藻でlの読み込みが抜けていたため設定追記

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ReservePostRequest;
+use App\Mail\ReserveMail;
 use App\Models\Reserve;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
