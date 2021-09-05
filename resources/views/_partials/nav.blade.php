5e16afd3a9a9d5f1bb6541b365f73219d2ef843a
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-05 18:29:55 +0900

    add: navbar 右上の会員登録周りのUIが認証状態にに応じていなかったので追加

--- resources/views/_partials/nav.blade.php
+++ resources/views/_partials/nav.blade.php
@@ -22,17 +22,30 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navDropdown" aria-controls="navDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse flex-row" id="navDropdown">
           <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle text-white" href="#" id="navDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="bi bi-person me-1"></i>
-                {{ Auth::guard('admin')->user()->name ?? ' ' }}
+
+            @if(Auth::guard('member')->check())
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="navDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="bi bi-person me-1"></i>
+                  {{ Auth::guard('admin')->user()->name ?? '' }}
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navDropdownMenuLink">
+                  <li><a class="dropdown-item text-white" href="#">マイページ</a></li>
+                </ul>
+              </li>
+            @else
+              <a class="nav-item nav-link navbar-menu text-white" href="{{ route('register.form') }}">
+                <div>ログイン</div>
               </a>
-              <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navDropdownMenuLink">
-                <li><a class="dropdown-item text-white" href="{{ route('admin.logout') }}">ログアウト</a></li>
-              </ul>
-            </li>
+
+              <a class="nav-item nav-link navbar-menu text-white" href="{{ route('register.form') }}">
+                <div>会員登録</div>
+              </a>
+            @endif
+
           </ul>
         </div>
       </div>
