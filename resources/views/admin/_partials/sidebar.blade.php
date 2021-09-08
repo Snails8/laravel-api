2b09ef3f1a445f1855040e90f301aac256205fbd
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-08 11:13:46 +0900

    fix: sidebar のuiヲイ使いやすく調整

--- resources/views/admin/_partials/sidebar.blade.php
+++ resources/views/admin/_partials/sidebar.blade.php
@@ -1,60 +1,80 @@
 <nav class="sidebar">
-  <ul class="nav flex-column accordion" id="nav_accordion">
-    <li class="nav-item">
-      <a class="nav-link" href="#">メニュー1</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link collapsed" data-bs-toggle="collapse" data-bs-target="#menu_item1" aria-controls="menu_item2" aria-expanded="true" href="#">
-        メニュー2<i class="bi bi-caret-down-fill ms-1"></i>
+
+  <p class="fw-bold px-3 mt-3 mb-0">利用会社マスタ</p>
+  <ul class="nav flex-column mb-2">
+    <li class="nav-item ps-2">
+      <a class="nav-link" href="">
+        <i class="bi bi-caret-right me-1"></i>利用会社一覧
       </a>
-      <ul id="menu_item1" class="submenu collapse accordion-collapse" data-bs-parent="#nav_accordion">
-        <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>サブメニュー1</a></li>
-        <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>サブメニュー2</a></li>
-        <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>サブメニュー3</a> </li>
-      </ul>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item2" aria-controls="menu_item2" aria-expanded="true" href="#">
-        メニュー3<i class="bi bi-caret-down-fill ms-1"></i>
+    <li class="nav-item ps-2">
+      <a class="nav-link" href="">
+        <i class="bi bi-caret-right me-1"></i>サンプル管理
       </a>
-      <ul id="menu_item2" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-        <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>サブメニュー1</a></li>
-        <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>サブメニュー2</a></li>
-        <li><a class="nav-link" href="#"><i class="bi bi-chevron-right me-1"></i>サブメニュー3</a></li>
-      </ul>
-    </li>
-      <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item2" aria-controls="menu_item2" aria-expanded="true" href="{{ route('admin.news.index') }}">
-              お知らせマスタ<i class="bi bi-caret-down-fill ms-1"></i>
-          </a>
-          <ul id="menu_item2" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-              <li><a class="nav-link" href="{{ route('admin.news.index') }}"><i class="bi bi-chevron-right me-1"></i>お知らせ管理</a></li>
-          </ul>
-          <ul id="menu_item2" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-              <li><a class="nav-link" href="{{ route('admin.news_category.index') }}"><i class="bi bi-chevron-right me-1"></i>お知らせカテゴリ管理</a></li>
-          </ul>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item3" aria-controls="menu_item2" aria-expanded="true" href="#">
-              自社情報マスタ<i class="bi bi-caret-down-fill ms-1"></i>
-          </a>
-          <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-              <li><a class="nav-link" href="{{ route('admin.company.edit', ['company' => 1 ]) }}"><i class="bi bi-chevron-right me-1"></i>自社情報管理</a></li>
-          </ul>
-          <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-              <li><a class="nav-link" href="{{ route('admin.office.index') }}"><i class="bi bi-chevron-right me-1"></i>支社管理</a></li>
-          </ul>
-          <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-              <li><a class="nav-link" href="{{ route('admin.user.index') }}"><i class="bi bi-chevron-right me-1"></i>ユーザー管理</a></li>
-          </ul>
-      </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item4" aria-controls="menu_item2" aria-expanded="true" href="{{ route('admin.hr_company.index') }}">
-        利用会社管理<i class="bi bi-caret-down-fill ms-1"></i>
-      </a>
-        <ul id="menu_item4" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-            <li><a class="nav-link" href="{{ route('admin.hr_company.index') }}"><i class="bi bi-chevron-right me-1"></i>利用会社管理</a></li>
-        </ul>
     </li>
   </ul>
+
+
+  <p class="fw-bold px-3 mt-3 mb-0">自社情報マスタ</p>
+  <ul class="nav flex-column mb-2">
+    <li class="nav-item ps-2">
+      <a class="nav-link" href="{{ route('admin.company.edit', ['company' => 1 ]) }}">
+        <i class="bi bi-caret-right me-1"></i>自社情報管理
+      </a>
+    </li>
+    <li class="nav-item ps-2">
+      <a class="nav-link" href="">
+        <i class="bi bi-caret-right me-1"></i>支社管理
+      </a>
+    </li>
+    <li class="nav-item ps-2">
+      <a class="nav-link" href="">
+        <i class="bi bi-caret-right me-1"></i>ユーザー管理
+      </a>
+    </li>
+  </ul>
+
+  <p class="fw-bold px-3 mt-3 mb-0">お知らせマスタ</p>
+  <ul class="nav flex-column mb-2">
+    <li class="nav-item ps-2">
+      <a class="nav-link" href="">
+        <i class="bi bi-caret-right me-1"></i>お知らせ管理
+      </a>
+    </li>
+    <li class="nav-item ps-2">
+      <a class="nav-link" href="">
+        <i class="bi bi-caret-right me-1"></i>導入事例
+      </a>
+    </li>
+  </ul>
+
+  <p class="fw-bold px-3 mt-3 mb-0">サンプル</p>
+  <ul class="nav flex-column accordion" id="nav_accordion">
+
+    <li class="nav-item ps-2">
+      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item3" aria-controls="menu_item2" aria-expanded="true" href="#">
+        登録情報管理<i class="bi bi-caret-down-fill ms-1"></i>
+      </a>
+      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
+        <li><a class="nav-link" href="{{ route('admin.company.edit', ['company' => 1 ]) }}"><i class="bi bi-chevron-right me-1"></i>お問い合わせ一覧</a></li>
+      </ul>
+      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
+        <li><a class="nav-link" href="{{ route('admin.office.index') }}"><i class="bi bi-chevron-right me-1"></i>支社管理</a></li>
+      </ul>
+      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
+        <li><a class="nav-link" href="{{ route('admin.user.index') }}"><i class="bi bi-chevron-right me-1"></i>ユーザー管理</a></li>
+      </ul>
+    </li>
+
+{{--    <li class="nav-item ps-2">--}}
+{{--      <a class="nav-link" href="#">メニュー1</a>--}}
+{{--    </li>--}}
+
+{{--    <li class="nav-item ps-2">--}}
+{{--      <a class="nav-link" href="">--}}
+{{--        <i class="bi bi-caret-right me-1"></i>自社情報管理--}}
+{{--      </a>--}}
+{{--    </li>--}}
+  </ul>
+
 </nav>
