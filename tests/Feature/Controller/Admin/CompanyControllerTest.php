a057c46d604dd861ae7b43dff6d363cb11b9ded4
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-08 09:55:34 +0900

    fix: eidt のみのテストに修正

--- tests/Feature/Controller/Admin/CompanyControllerTest.php
+++ tests/Feature/Controller/Admin/CompanyControllerTest.php
@@ -15,29 +15,7 @@ class CompanyControllerTest extends TestCase
     /**
      * @test
      */
-    public function 会社管理一覧画面のレスポンスは正常である()
-    {
-        $user = User::factory()->create();
-
-        $this->actingAs($user, 'admin')
-            ->get(route('admin.company.index'))->assertStatus(200);
-    }
-
-    /**
-     * @test
-     */
-    public function 会社管理作成画面のレスポンスは正常である()
-    {
-        $user = User::factory()->create();
-
-        $this->actingAs($user, 'admin')
-            ->get(route('admin.company.create'))->assertStatus(200);
-    }
-
-    /**
-     * @test
-     */
-    public function 会社管理の編集画面のレスポンスは正常である()
+    public function 会社管理編集画面のレスポンスは正常である()
     {
         $user = User::factory()->create();
         $companyId = Company::query()->first('id');
@@ -50,37 +28,10 @@ class CompanyControllerTest extends TestCase
     /**
      * @test
      */
-    public function 会社管理の新規登録処理のレスポンスは正常である()
-    {
-        $user = User::factory()->create();
-        // formDataの用意
-        $postData = [
-            'name'          => 'テスト',
-            'zipcode1'      => 000,
-            'zipcode2'      => 1111,
-            'address'       => 'テスト',
-            'address_other' => '',
-            'tel'           => 00011112222,
-            'email'         => 'sample@sample.com',
-            'representative_name' => 'テスト',
-        ];
-
-        $res = $this->actingAs($user, 'admin')
-            // fromでリファラーを指定しないとテストの仕様上、発見できずback()でホームに飛ばされる
-            ->from(route('admin.company.index'))
-            ->post(route('admin.company.store'), $postData);
-
-        $res->assertRedirect(route('admin.company.index'));
-    }
-
-    /**
-     * @test
-     */
     public function 会社管理のアップデート処理のレスポンスは正常である()
     {
         $user = User::factory()->create();
-        // putなのでデータ取得用に
-        $updateCompany = Company::query()->inRandomOrder()->first();
+        $companyId = Company::query()->first('id');
 
         $postData = [
             'name'          => 'テスト',
@@ -90,13 +41,13 @@ class CompanyControllerTest extends TestCase
             'address_other' => '',
             'tel'           => 00011112222,
             'email'         => 'sample@sample.com',
-            'representative_name' => 'テスト',
+            'ceo'           => 'テスト',
         ];
 
         $res = $this->actingAs($user, 'admin')
-            ->from(route('admin.company.index'))
-            ->put(route('admin.company.update', ['company' => $updateCompany->id]), $postData);
+            ->from(route('admin.company.edit', ['company' => $companyId ]))
+            ->put(route('admin.company.update', ['company' => $companyId]), $postData);
 
-        $res->assertRedirect(route('admin.company.index'));
+        $res->assertRedirect(route('admin.company.edit', ['company' => $companyId]));
     }
 }
