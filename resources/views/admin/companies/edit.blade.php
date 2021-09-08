bd3dea54b13e17c252c4a98ff44156ff8c026c99
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-08 15:21:50 +0900

    delete: 不要なスタイルの削除

--- resources/views/admin/companies/edit.blade.php
+++ resources/views/admin/companies/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('admin._layouts.app')
 @section('title', $title)
 @section('content')
-  <h4 class="c-grey-900 mT-10 mB-30">{{ $title }}</h4>
   @includeWhen(session('flash_message'), 'admin._partials.flash_message_success')
   {{ Form::open(['route' => ['admin.company.update', 'company' => $company->id], 'method' => 'put']) }}
   @csrf
