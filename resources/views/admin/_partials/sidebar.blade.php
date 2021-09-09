b6e916d89f29efe72567143f9e6f27128b3565bb
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-09 21:36:03 +0900

    add: sidebar の動線をリファクタリング

--- resources/views/admin/_partials/sidebar.blade.php
+++ resources/views/admin/_partials/sidebar.blade.php
@@ -23,12 +23,12 @@
       </a>
     </li>
     <li class="nav-item ps-2">
-      <a class="nav-link" href="">
+      <a class="nav-link" href="{{ route('admin.office.index') }}">
         <i class="bi bi-caret-right me-1"></i>事務所管理
       </a>
     </li>
     <li class="nav-item ps-2">
-      <a class="nav-link" href="">
+      <a class="nav-link" href="{{ route('admin.user.index') }}">
         <i class="bi bi-caret-right me-1"></i>ユーザー管理
       </a>
     </li>
@@ -67,23 +67,23 @@
     </li>
   </ul>
 
-  <p class="fw-bold px-3 mt-3 mb-0">サンプル</p>
-  <ul class="nav flex-column accordion" id="nav_accordion">
-
-    <li class="nav-item ps-2">
-      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item3" aria-controls="menu_item2" aria-expanded="true" href="#">
-        登録情報管理<i class="bi bi-caret-down-fill ms-1"></i>
-      </a>
-      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-        <li><a class="nav-link" href="{{ route('admin.company.edit', ['company' => 1 ]) }}"><i class="bi bi-chevron-right me-1"></i>お問い合わせ一覧</a></li>
-      </ul>
-      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-        <li><a class="nav-link" href="{{ route('admin.office.index') }}"><i class="bi bi-chevron-right me-1"></i>事務所管理</a></li>
-      </ul>
-      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">
-        <li><a class="nav-link" href="{{ route('admin.user.index') }}"><i class="bi bi-chevron-right me-1"></i>ユーザー管理</a></li>
-      </ul>
-    </li>
+{{--  <p class="fw-bold px-3 mt-3 mb-0">サンプル</p>--}}
+{{--  <ul class="nav flex-column accordion" id="nav_accordion">--}}
+{{----}}
+{{--    <li class="nav-item ps-2">--}}
+{{--      <a class="nav-link" data-bs-toggle="collapse" data-bs-target="#menu_item3" aria-controls="menu_item2" aria-expanded="true" href="#">--}}
+{{--        登録情報管理<i class="bi bi-caret-down-fill ms-1"></i>--}}
+{{--      </a>--}}
+{{--      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">--}}
+{{--        <li><a class="nav-link" href="{{ route('admin.company.edit', ['company' => 1 ]) }}"><i class="bi bi-chevron-right me-1"></i>お問い合わせ一覧</a></li>--}}
+{{--      </ul>--}}
+{{--      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">--}}
+{{--        <li><a class="nav-link" href="{{ route('admin.office.index') }}"><i class="bi bi-chevron-right me-1"></i>事務所管理</a></li>--}}
+{{--      </ul>--}}
+{{--      <ul id="menu_item3" class="submenu collapse accordion-collapse show" data-bs-parent="#nav_accordion">--}}
+{{--        <li><a class="nav-link" href="{{ route('admin.user.index') }}"><i class="bi bi-chevron-right me-1"></i>ユーザー管理</a></li>--}}
+{{--      </ul>--}}
+{{--    </li>--}}
 
 {{--    <li class="nav-item ps-2">--}}
 {{--      <a class="nav-link" href="#">メニュー1</a>--}}
