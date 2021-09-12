22a0d27166bbf245a6eb02e0b6d2189c6d11e4c7
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-12 17:30:47 +0900

    add: validation message の追加

--- app/Http/Requests/ContactPostRequest.php
+++ app/Http/Requests/ContactPostRequest.php
@@ -28,20 +28,39 @@ class ContactPostRequest extends FormRequest
     public function rules(): array
     {
         $rules = [
-            'company' => 'required|max:255',
-            'name' => 'required|max:255',
-            'email' => 'required|email',
-            'tel' => 'required|regex:/^[0-9]{2,4}[0-9]{2,4}[0-9]{3,4}$/',
-            'employee_count' => 'required|integer',
-            'contact_type' => 'required|integer',
-            'detail' => 'required',
+            'company'        => 'required|max:255',
+            'name'           => 'required|max:255',
+            'email'          => 'required|email',
+            'tel'            => 'required|regex:/^[0-9]{2,4}[0-9]{2,4}[0-9]{3,4}$/',
+            'employee_count' => 'integer|min:1',
+            'contact_type'   => 'integer|min:1',
+            'detail'         => 'required',
         ];
 
         return $rules;
     }
 
-    public function messages()
+    /**
+     * @return string[]
+     */
+    public function messages(): array
     {
+        $messages = [
+            'company.required'           => '名前は必ず入力してください。',
+            'company.max'                => '255文字以内で入力してください。',
+            'name.required'              => '名前は必ず入力してください。',
+            'name.max'                   => '255文字以内で入力してください。',
+            'email.required'             => 'メールアドレスは必ず入力してください。',
+            'email.email'                => '有効なメールアドレスの形式ではありません。',
+            'tel.required'               => '電話番号は必ず入力してください。',
+            'tel.regex'                  => '有効な電話番号の形式ではありません。',
+            'employee_count.integer'     => '従業員数の入力値が有効な形式ではありません。',
+            'employee_count.min'         => '従業員数は必ず選択してください。',
+            'contact_type.integer'       => '入力値が有効な形式ではありません。',
+            'contact_type.min'           => '必ず選択してください。',
+            'detail.required'            => '必ず入力してください。',
+        ];
 
+        return $messages;
     }
 }
