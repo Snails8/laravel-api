5744b678cd9152f29c5766a60f13e3abbc169ffc
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-15 21:13:06 +0900

    add: サービス契約会社管理との動線作成

--- resources/views/admin/_partials/sidebar.blade.php
+++ resources/views/admin/_partials/sidebar.blade.php
@@ -3,7 +3,7 @@
   <p class="fw-bold px-3 mt-3 mb-0">利用会社マスタ</p>
   <ul class="nav flex-column mb-2">
     <li class="nav-item ps-2">
-      <a class="nav-link" href="">
+      <a class="nav-link" href="{{ route('admin.hr_company.index') }}">
         <i class="bi bi-caret-right me-1"></i>利用会社一覧
       </a>
     </li>
