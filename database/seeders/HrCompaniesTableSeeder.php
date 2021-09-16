b00eacf4b06529193cd26fb36b617e8aae36b1a4
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-17 08:53:35 +0900

    add: 開発しやすくなるようにtestData の調整

--- database/seeders/HrCompaniesTableSeeder.php
+++ database/seeders/HrCompaniesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\HrCompany;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class HrCompaniesTableSeeder extends Seeder
 {
@@ -14,6 +15,31 @@ class HrCompaniesTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('hr_companies')->insert([
+            [
+                'name'    => 'sample name',
+                'zipcode' => 1112222,
+                'address' => 'sample address',
+                'address_other' => 'sample_address_other',
+                'tel'     => 11122223333,
+                'email'   => 'sample@test.com',
+                'representative' => 'sample representative',
+                'role'    => 'Free',
+                'is_contract' => true,
+            ],
+            [
+                'name'    => 'sample name2',
+                'zipcode' => 1112222,
+                'address' => 'sample address2',
+                'address_other' => 'sample_address_other2',
+                'tel'     => 11122223333,
+                'email'   => 'sample2@test.com',
+                'representative' => 'sample representative2',
+                'role'    => 'Standard',
+                'is_contract' => true,
+            ],
+        ]);
+
         HrCompany::factory(20)->create();
     }
 }
