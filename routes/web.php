7389286fe475bdf33ce09dd869279f455811ecad
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-16 08:46:04 +0900

    add: 今後機能を追加する要諦のルーティング(コメント)の作成

--- routes/web.php
+++ routes/web.php
@@ -23,21 +23,42 @@ use App\Http\Controllers\ContactController;
 Route::get('/', [TopController::class, 'index'])->name('top');
 
 //Auth::routes();
+// 概要 HRT とは
 
+
+// 機能
+
+
+// 導入の流れ
+
+
+// 料金(プラン)
+
+
+// 導入事例
+
+
+// お知らせイベント
 Route::prefix('news')->group(function () {
     Route::get('/', [NewsController::class, 'index'])->name('news.index');
     Route::get('/{newsId}', [NewsController::class, 'show'])->where('newsId', '[0-9]+')->name('news.show');
 });
 
-Route::prefix('register')->group(function () {
-    Route::get('/', [RegisterController::class, 'showForm'])->name('register.form');
-    Route::post('/', [RegisterController::class, 'submit'])->name('register.post');
-    Route::get('/thanks', [RegisterController::class, 'showThanks'])->name('register.thanks');
-});
+// もっと知りたい
 
+
+// お問い合わせ
 Route::prefix('contacts')->group(function () {
     Route::get('/', [ContactController::class, 'showForm'])->name('contact.index');
     Route::post('/', [ContactController::class, 'submit'])->name('contact.post');
     Route::get('/thanks', [ContactController::class, 'showThanks'])->name('contact.thanks');
+});
+
+
+// 無料お試し
+Route::prefix('register')->group(function () {
+    Route::get('/', [RegisterController::class, 'showForm'])->name('register.form');
+    Route::post('/', [RegisterController::class, 'submit'])->name('register.post');
+    Route::get('/thanks', [RegisterController::class, 'showThanks'])->name('register.thanks');
 });
 
