3b377157f4c9eeccea97f6f6505d96234dbfa5ed
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-15 15:30:28 +0900

    add: queue についてコメントを追加

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Queue でコケたJob がココに保存される
+ * defaultで作成されるファイル
+ */
 class CreateFailedJobsTable extends Migration
 {
     /**
