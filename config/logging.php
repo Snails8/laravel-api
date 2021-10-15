217526dc17def52285e634f2fef502b749262153
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-15 20:48:38 +0900

    add: slack 通知用にlog のalert のレベルを変更

--- config/logging.php
+++ config/logging.php
@@ -57,9 +57,9 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
+            'username' => 'Sample User Name',
             'emoji' => ':boom:',
-            'level' => env('LOG_LEVEL', 'critical'),
+            'level' => 'error',
         ],
 
         'papertrail' => [
