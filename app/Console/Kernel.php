b01f14ba1d94cb072fb39c31967df192cdba9db5
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-15 20:27:13 +0900

    add: メール定時処理Sampleの作成

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,9 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+/**
+ * 定時処理
+ */
 class Kernel extends ConsoleKernel
 {
     /**
@@ -24,6 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('mail:sample')->dailyAt('12:00')->timezone('Asia/Tokyo');
         // $schedule->command('inspire')->hourly();
     }
 
