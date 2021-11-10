3816ddfbae7fe89b038f21d8c477fc2bc6fe19e2
S-tanishi <tanichi.u.n.20@gmail.com>
2021-11-10 21:38:21 +0900

    add: validation の作成

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Mail\RegisterMail;
 use App\Models\HrCompany;
+use App\Http\Requests\ReservePostRequest;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
