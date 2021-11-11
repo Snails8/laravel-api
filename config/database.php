ee5fab34544bb1b3f9a12b0a01a14dac71b0db3f
S-tanishi <tanichi.u.n.20@gmail.com>
2021-11-11 11:34:34 +0900

    add: redisにqueueを流し込む環境情報を追記

--- config/database.php
+++ config/database.php
@@ -171,6 +171,21 @@ return [
             ],
         ],
 
+        // 追記:  redisにqueueを流し込む環境設定
+        'queue' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_QUEUE_DB', 2),
+            'ssl'    => [
+                'verify_peer' => false,
+                'verify_peer_name'  => false,
+                'allow_self_signed' => true,
+            ],
+        ],
+
     ],
 
 ];
