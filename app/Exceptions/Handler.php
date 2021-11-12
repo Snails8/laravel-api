d680df49e3c467d0fe8f72ed4291f442e6e2de94
S-tanishi <tanichi.u.n.20@gmail.com>
2021-11-12 10:19:36 +0900

    add: マルチ認証の際に認証例外時にredirectするrouteが壽福していたのでlogin 先を分ける処理の作成

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -28,14 +30,41 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
+     * Report or log an exception.
+     * @param Throwable $exception
+     * @throws Throwable
      */
-    public function register()
+    public function report(Throwable $exception)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     * @param  Request  $request
+     * @param Throwable $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        return parent::render($request, $exception);
+    }
+
+    /**
+     * 管理画面と表の認証例外時にそれぞれ応じた/login に飛ばす処理
+     *
+     * Convert an authentication exception into an unauthenticated response.
+     * @param Request $request
+     * @param  AuthenticationException  $exception
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        $redirectTo = in_array('admin', $exception->guards(), true) ? 'admin.login' : 'login';
+
+        return $request->expectsJson()
+            ? response()->json(['message' => $exception->getMessage()], 401)
+            : redirect()->guest(route($redirectTo));
     }
 }
