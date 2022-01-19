8e3d0a2ce58509cc8853579b123fb5e1a7a11877
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-19 21:27:25 +0900

    add: json の検証ができるようにメソッド修正

--- app/Http/Controllers/Api/HrAdmin/RestControllerController.php
+++ app/Http/Controllers/Api/HrAdmin/RestControllerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 /**
  * resr api の簡易版 json の挙動検証用
@@ -14,9 +15,36 @@ class RestControllerController extends Controller
 {
     public function index(): JsonResponse
     {
+        $data = $this->getUsers();
+//        $data = $this->getArrayUsers();
+
+        return response()->json($data, 200);
+    }
+
+    /**
+     * @return Collection
+     */
+    private function getUsers(): Collection
+    {
+        return User::query()->get();
+    }
+
+    /**
+     * @return array
+     */
+    private function getArrayUsers(): array
+    {
+        $text = 'sample text';
         $users = User::query()->get();
 
-        return response()->json($users, 200);
+        $data = [
+            'users' => $users,
+            'text' => $text,
+        ];
+
+        return $data;
+    }
+}
 
         // front 側では以下のように受け取れる
 //  return {
@@ -42,5 +70,23 @@ class RestControllerController extends Controller
 //    }
 //  ]
 //
-    }
-}
+
+
+//{
+//    users: [
+//    {
+//        id: 1,
+//      name: 'たにし',
+//      kana: 'タニシ',
+//      email: 'sample@gmail.com',
+//      email_verified_at: '2021-11-12T00:30:40.000000Z',
+//      role: 'master',
+//      post: '',
+//      office_id: 1,
+//      created_at: '2021-11-12T00:30:40.000000Z',
+//      updated_at: '2021-11-12T00:30:40.000000Z'
+//    }
+//  ],
+//  text: 'sample text'
+//}
+
