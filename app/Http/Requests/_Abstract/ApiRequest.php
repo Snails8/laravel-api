3ffcc59c78e465b1ef966ad332aa61c919997d7a
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-26 21:28:16 +0900

    add: api validation 用のabstract class 作成

---
+++ app/Http/Requests/_Abstract/ApiRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\_Abstract;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+/**
+ * 抽象クラス
+ */
+abstract class ApiRequest extends FormRequest
+{
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  Validator  $validator
+     * @return void
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $data = [
+            'message' => __('The given data was invalid.'),
+            'errors'  => $validator->errors()->toArray(),
+        ];
+
+        throw new HttpResponseException(response()->json($data, 422));
+    }
+}
+
+// __(string)はローカライズ考慮
+// localeの制御や、言語ファイルの準備などが必要
+
+// config/app.php
+//：
+//    'locale' => 'ja',
+//：
+
+// resources/lang/ja.json
+//{
+//    ：
+//    "The given data was invalid.": "送信データのチェックで不備が見つかりました。",
+//    ：
+//}
