31f482992f277c300f5b8fd2180565685e5fbf0d
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-26 20:27:32 +0900

    add: blog API 作成処理 作成

--- app/Http/Controllers/Api/HrAdmin/Rest/CreateController.php
+++ app/Http/Controllers/Api/HrAdmin/Rest/CreateController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api\HrAdmin\Rest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CreateController extends Controller
 {
-    //
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        Log::debug($request);
+
+        $blog = Blog::create($request->all());
+
+        return $blog
+            ? response()->json($blog, 201)
+            : response()->json([], 500);
+    }
 }
