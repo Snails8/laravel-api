c3bb1cdcf2b9a65008f6cb66b0e66af491c79b47
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-30 16:30:46 +0900

    add: 中間テーブルの作成

--- database/migrations/2022_01_30_161600_create_work_work_tag_table.php
+++ database/migrations/2022_01_30_161600_create_work_work_tag_table.php
@@ -14,9 +14,21 @@ class CreateWorkWorkTagTable extends Migration
     public function up()
     {
         Schema::create('work_work_tag', function (Blueprint $table) {
+            // 主キーは複合主キーでもいいかも
             $table->bigIncrements('id');
+            // 複合主キーを定義
+//            $table->primary(['work_id','work_tag_id']);
+
+            // カラム
             $table->bigInteger('work_id')->unsigned();
             $table->bigInteger('work_tag_id')->unsigned();
+//            $table->unsignedBigInteger('work_id');    // やっていることは同じ
+//            $table->unsignedBigInteger('tag_id');
+
+            // 指定したカラムに外部キー制約を定義
+            $table->foreign('work_id')->references('id')->on('works')->onDelete('cascade');
+            $table->foreign('work_tag_id')->references('id')->on('work_tags')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
