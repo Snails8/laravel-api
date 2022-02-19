0b35faac404c2c7d9333500c119a73f62ed68e77
Snails8 <snails8d@gmail.com>
2022-02-19 09:42:33 +0900

    add: vue-js-datepicker のcomponent作成

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,6 +19,10 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+// partials
+Vue.component('date-picker-component', require('./components/_partials/DatePickerComponent').default)
+
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 Vue.component('date-picker-component', require('./components/DatePickerFormComponent.vue').default);
