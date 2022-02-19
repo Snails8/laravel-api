2dbb0dbbef652d9700910a3167245171c7e2a320
Snails8 <snails8d@gmail.com>
2022-02-19 11:06:46 +0900

    fix: app.jsの読み込み回りの調整

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,12 +20,11 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 // partials
-Vue.component('date-picker-component', require('./components/_partials/DatePickerComponent').default)
+Vue.component('date-picker-component', require('./_templates/DatePickerComponent').default)
 
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-Vue.component('date-picker-component', require('./components/DatePickerFormComponent.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
